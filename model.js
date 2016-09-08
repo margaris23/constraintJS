c7ed8aa6981a38b51c6cbe8c7c77e9d5c563e41b
Nikolaos Margaris <reverence23@gmail.com>
2016-09-08 20:12:09 +0300

    Reset model:redesign neede

--- model.js
+++ model.js
@@ -7,51 +7,18 @@
  * @constructor Constraint
  * @description creates the main constraint object
  */
-var Constraint = function (valFn, numOfArgs) {
-    'use strict';
-    this.check = valFn;
-    this.noa = numOfArgs;	// for now lets consider them valid
+var Constraint = function Contraint(valFn, numOfArgs) {
 };
 
  /**
   * @module ConstraintBuilder
   */
 var ConstraintBuilder = {
-    build: function (valFn, numOfArgs) {
+    build: function build(valFn, numOfArgs) {
         'use strict';
 
-        if (valFn === undefined || valFn === null || typeof(valFn) !== 'function') {
-            throw new Error('Undefined constraint function');
-        }
-
-        // 'check' propery will always be called by validators
-        // Object.defineProperty(Constraint, 'check', {
-        // 	enumerable: true,
-        // 	configurable: false,
-        // 	writable: false
-        // });
-
-        return Object.seal(new Constraint(valFn, numOfArgs));
     }
 };
 
-// Argument Object:
-// it must be minimal and not extendable
-/*var Argument = function (value) {
-  this.value = value;
-};
-
-var ArgumentBuilder = {
-  build: function (value) {
-    return Object.seal(new Argument(value));
-  }
-};
-
-function isArgument (obj) {
-  return obj !== undefined && obj !== null &&
-    obj instanceof Argument;
-}
-
-exports.isArgument = isArgument;*/
+// LETS START FROM THE BEGINNING!!!
 exports.ConstraintBuilder = ConstraintBuilder;
-//exports.ArgumentBuilder = ArgumentBuilder;
